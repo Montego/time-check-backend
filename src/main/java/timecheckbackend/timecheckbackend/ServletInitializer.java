dfc8f0ebca03d67ca3390e9d32c1b65e80654dca
Montego_RD <bar_d91@mail.ru>
2019-04-17 11:46:26 +0300

    Deliver stage added

---
+++ src/main/java/timecheckbackend/timecheckbackend/ServletInitializer.java
@@ -0,0 +1,13 @@
+package timecheckbackend.timecheckbackend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TimeCheckBackendApplication.class);
+    }
+
+}
