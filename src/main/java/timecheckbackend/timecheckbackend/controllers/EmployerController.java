b988274dd9f0935b6c45e0f00e454193d0b35ceb
Montego_RD <bar_d91@mail.ru>
2019-04-17 13:47:01 +0300

    10.1.20.23:5432/checktime

--- src/main/java/timecheckbackend/timecheckbackend/controllers/EmployerController.java
+++ src/main/java/timecheckbackend/timecheckbackend/controllers/EmployerController.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import timecheckbackend.timecheckbackend.Util.ApiResponse;
+//import timecheckbackend.timecheckbackend.Util.ApiResponse;
 import timecheckbackend.timecheckbackend.entities.Employer;
 import timecheckbackend.timecheckbackend.payloads.EmployerFullnameResponse;
 import timecheckbackend.timecheckbackend.payloads.EmployerResponse;
